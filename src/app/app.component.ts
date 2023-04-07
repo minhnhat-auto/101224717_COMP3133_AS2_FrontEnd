d5d6402e5e2ac608bc09bc9cb003daa3b6bf88a1
Nhat Minh Vo <nhat@nhats-mbp.home>
2023-04-06 23:47:43 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '101224717_comp3133_as2';
+}
